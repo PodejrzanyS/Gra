00c389c5e7e00378b6621c579ca751061642aecc
PodejrzanyS <d.sporek@wp.pl>
2021-03-12 21:42:58 +0100

    muzyka bez slidera

---
+++ Endless Game/Assets/Scripts/MuzykaSceny.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzykaSceny : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("Muzyka");
+        if (musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
---
+++ Endless Game/Assets/Scripts/MuzykaSlider.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuzykaSlider : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public GameObject ObjectMusic;
+
+    private float MusicVolume = 0f;
+    private AudioSource AudioSource;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ObjectMusic = GameObject.FindWithTag("Muzyka");
+        AudioSource = ObjectMusic.GetComponent<AudioSource>();
+
+        MusicVolume = PlayerPrefs.GetFloat("Volume");
+        AudioSource.volume = MusicVolume;
+        volumeSlider.value = MusicVolume;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        AudioSource.volume = MusicVolume;
+        PlayerPrefs.SetFloat("Volume", MusicVolume);
+    }
+
+    public void VolumeUpdater(float volume)
+    {
+        MusicVolume = volume;
+    }
+
+    public void MusicReset()
+    {
+        PlayerPrefs.DeleteKey("Volume");
+        AudioSource.volume = 1;
+        volumeSlider.value = 1;
+    }
+}
